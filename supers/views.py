0e7d5508386c3d35c8b21bab035b0908179eb3a1
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-15 14:54:21 -0500

    Created SuperType Serializer + Views + Urls + Post Functionality

--- supers/views.py
+++ supers/views.py
@@ -28,17 +28,17 @@ def supers_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
-    super = get_object_or_404(Super, pk=pk)
+    supers = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
-        serializer = SuperSerializer(super);
+        serializer = SuperSerializer(supers);
         return Response(serializer.data)
 
     elif request.method == 'PUT':
-        serializer = SuperSerializer(super, data=request.data)
+        serializer = SuperSerializer(supers, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'DELETE':
-        super.delete()
+        supers.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
