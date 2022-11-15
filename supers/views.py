dc9c4af0905436f1764ab803a617a6ba31802576
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-15 17:28:16 -0500

    Refactored Custome Param + Uploaded Post Json + New ERD Diagram

--- supers/views.py
+++ supers/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 from .models import SuperType
+
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
@@ -15,10 +16,24 @@ def supers_list(request):
         if super_type_type:
             queryset = queryset.filter(super_type__type=super_type_type)
         
-        
-        serializer = SuperSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        
+            serializer = SuperSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'GET':
+            super_types = SuperType.objects.all()
+
+            custom_response = {}
+
+            for super_type in super_types:
+
+                supers = Super.objects.filter(super_type_id=super_type.id)
+
+                super_serializer = SuperSerializer(supers, many=True)
+
+                custom_response[super_type.type] = {
+                    "Super": super_serializer.data,         
+                }
+            return Response(custom_response)
 
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
@@ -26,24 +41,6 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
-def supers_list(request):
-
-    super_types = SuperType.objects.all()
-
-    custom_response = {}
-
-    for super_type in super_types:
-
-        supers = Super.objects.filter(super_type_id=super_type.id)
-
-        super_serializer = SuperSerializer(supers, many=True)
-
-        custom_response[super_type.type] = {
-             "Super": super_serializer.data,         
-        }
-    
-    return Response(custom_response)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
