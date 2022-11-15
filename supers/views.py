436992e831daf924efddf3d61efb23f0f0c0a9ec
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-15 16:51:51 -0500

    Created Custom Param Function for Post API

--- supers/views.py
+++ supers/views.py
@@ -4,27 +4,46 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
-
+from .models import SuperType
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-
-        super_type_type = request.query_params.get('type')
+        super_type_type = request.query_params.get('type') #By parameter
         print(super_type_type)
-
         queryset = Super.objects.all()
-
+        
         if super_type_type:
             queryset = queryset.filter(super_type__type=super_type_type)
         
+        
         serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+        
 
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def supers_list(request):
+
+    super_types = SuperType.objects.all()
+
+    custom_response = {}
+
+    for super_type in super_types:
+
+        supers = Super.objects.filter(super_type_id=super_type.id)
+
+        super_serializer = SuperSerializer(supers, many=True)
+
+        custom_response[super_type.type] = {
+             "Super": super_serializer.data,         
+        }
+    
+    return Response(custom_response)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
