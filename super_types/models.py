a3768c01798160e3da9962c9a1f983f8fdf5d671
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-15 12:12:36 -0500

    Views file +Post Functionality Created

--- super_types/models.py
+++ super_types/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class SuperType(models.Model):
-    super_type = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+
+        super_type_type = request.query_params.get('type')
+        print(super_type_type)
+
+        queryset = Super.objects.all()
+
+        if super_type_type:
+            queryset = queryset.filter(super_type__type=super_type_type)
+        
+        serializer = SuperSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
